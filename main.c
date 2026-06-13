4b424ebb5b94b823d55dc3779910ce6b80df8ac4
Aidan Hamilton <aidan193@cs.ucsb.edu>
2026-06-12 19:56:59 -0700

    Fixed parsing to account for different types of whitespace

--- main.c
+++ main.c
@@ -2,30 +2,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define MAX_LINE 101
-#define SPACE ' '
+#define MAX_ARGS MAX_LINE
+#define MAX_CMDS MAX_LINE
+
 
 void test_output(const char* cmd) {
-    fprintf(stdout, "Processed Input: %s\n", cmd);
+    fprintf(stdout, "Processed Input: \"%s\"\n", cmd);
 }
 
 void parse_and_run_command(const char* command) {
-    char commands_to_execute[MAX_LINE][MAX_LINE];
-    size_t command_count = 0;
+    char tokens[MAX_LINE][MAX_LINE];
+    size_t token_count = 0;
 
     char token[MAX_LINE];    
-    size_t token_len = 0, command_len = strlen(command);	    
+    size_t token_len = 0; 
+    size_t command_len = strlen(command);	    
      
     /*  Parse through command input
         Tokenize each command
         Append command to execution buffer*/
     for (size_t pos = 0; pos < command_len; pos++) {
        
+        char c = command[pos];
+
+        if (c == '\0' || c == ' ' || c == '\t' || c == '\v') {
+
+            if (token_len > 0) {
+                token[token_len] = '\0';
+
+                strcpy(tokens[token_count], token);
+                token_count++;
+
+                token_len = 0;
+            }
+
+            if (c == '\0') break;
+
+        } else {
+            token[token_len++] = c;
+        }
+
+        if (token_count == 0) {
+            fprintf(stderr, "Invalid command\n");
+            return;
+        }
+
         // Begin reading after parsing whitespace 
-        if (command[pos] != SPACE) {
+        if (command[pos] != ' ') {
             
-            // Write to token until next occurence of whitespace 
             while(command[pos] != SPACE && command[pos] != '\0') {
                 token[token_len] = command[pos];
                 token_len++;
@@ -45,23 +73,24 @@ void parse_and_run_command(const char* command) {
     }
 
     if (command_count > 0) {
+        char output_buffer[MAX_LINE];
         
-        /*  Execute commands from buffer in sequential order
+        /*  Execute commands from commmands_to_execute in sequential order
             
         */
         for (size_t it = 0; it < command_count; it++) {
-            
+            pid_t pid = fork();
+
+            const char* cmd = commands_to_execute[it];
             // Test if command is being read properly
-            test_output(commands_to_execute[it]);
+            test_output(cmd);
             
-            if (strcmp(commands_to_execute[it], "exit") == 0) {
-                
+            if (strcmp(cmd, "exit") == 0) {
                 exit(0);
-            } else {
-                fprintf(stderr, "Invalid command\n");
-            }
+            } else if (strcmp(cmd, "echo")) {
+                
 
-
+            } else { fprintf(stderr, "Invalid command\n"); }
         }
         
     } else {
